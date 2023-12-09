24b1668294b4669796545a3f2b4a940823d01e40
Aditya <akkpk933@gmail.com>
2023-12-09 13:28:44 +0530

    Modified Added

--- scripts/amazone.js
+++ scripts/amazone.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
             </div>
@@ -54,10 +54,11 @@ products.forEach((product) => {
     `;
 });
 
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  let addedMessageTimeoutId;
+
   button.addEventListener('click', () => {
     // const productId = button.dataset.productId;
     const {productId} = button.dataset;
@@ -90,5 +91,20 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedMessage.classList.add('added-to-cart-visible');
+
+    setTimeout(()=> {
+      if(addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+
+      addedMessageTimeoutId = timeoutId;
+    });
   });
 });
